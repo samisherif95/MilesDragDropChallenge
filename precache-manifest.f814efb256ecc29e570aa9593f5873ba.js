54bd4934e2e2ff2f340544df7ff380007cabcbe9
Sami Ellaboudy <sami.sherif95@gmail.com>
2020-06-12 21:22:37 -0700

    Updates

---
+++ precache-manifest.f814efb256ecc29e570aa9593f5873ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4bf678e607e09b4c73f338b7389ef10",
+    "url": "/MilesDragDropChallenge/index.html"
+  },
+  {
+    "revision": "806cd5440f1560cae589",
+    "url": "/MilesDragDropChallenge/static/css/main.e4a952ee.chunk.css"
+  },
+  {
+    "revision": "3f48ea906012f94710d3",
+    "url": "/MilesDragDropChallenge/static/js/2.890a6f2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MilesDragDropChallenge/static/js/2.890a6f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806cd5440f1560cae589",
+    "url": "/MilesDragDropChallenge/static/js/main.c116668d.chunk.js"
+  },
+  {
+    "revision": "8404f31060b93ae616ec",
+    "url": "/MilesDragDropChallenge/static/js/runtime-main.8b858f7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MilesDragDropChallenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
